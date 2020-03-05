53f3bbd4c039b19aa23d83a0965c9b1549304cb1
niki-01 <18622989+nikimanoledaki@users.noreply.github.com>
2020-03-05 15:55:34 +0000

    Adds gitignore and django-heroku in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for backend project.
 
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
