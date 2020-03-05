080528e6fa0d8e47d23189450853584f755fedf1
Meghan <meghan.e.mccue@gmail.com>
2020-03-05 14:16:13 +0000

    Add cors

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import BotOutput
+from .models import UserInput
+
 
 def index(request):
+  user_input = UserInput.objects.all()[0].text
   output = chat()
-  return HttpResponse(output)
+  return HttpResponse(output + user_input)
 
 def chat():
-  output = BotOutput.objects.create(text='I am a robot')
-  return output
+  return 'hello '
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
