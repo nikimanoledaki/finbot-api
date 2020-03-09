7f8a67c6631c41973a18937eaba68de8698e41ba
niki-01 <18622989+nikimanoledaki@users.noreply.github.com>
2020-03-09 16:27:12 +0000

    Add CSRF Exempt to views

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import UserInput
 from . import bot
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt 
 
 def index(request):
   user_input = list(UserInput.objects.all())[-1].text
