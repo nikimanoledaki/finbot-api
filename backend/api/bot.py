cd6030a05cec520456ca02a6676d24c176b56dfd
Meghan <meghan.e.mccue@gmail.com>
2020-03-09 09:21:01 +0000

    Additional Intents

--- backend/api/bot.py
+++ backend/api/bot.py
@@ -70,6 +70,7 @@ tensorflow.reset_default_graph()
 net = tflearn.input_data(shape=[None, len(training[0])])
 net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
 net = tflearn.fully_connected(net, len(output[0]), activation='softmax')
 net = tflearn.regression(net)
 
