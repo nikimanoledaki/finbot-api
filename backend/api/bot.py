2a4ae78f689d44b4b2fe9b78093b9b9a2eac82d8
niki-01 <18622989+nikimanoledaki@users.noreply.github.com>
2020-03-13 07:18:20 +0000

    Adds intents

--- backend/api/bot.py
+++ backend/api/bot.py
@@ -21,59 +21,59 @@ links_file_path = os.path.join(module_dir, 'intentsLinks.json')
 with open(links_file_path) as file:
   linksData = json.load(file)
 
-try:
-  with open('data.pickle', 'rb') as f:
-    words, labels, training, output = pickle.load(f)
+# try:
+#   with open('data.pickle', 'rb') as f:
+#     words, labels, training, output = pickle.load(f)
 
-except:
-  words = []
-  labels = []
-  docs_x = []
-  docs_y = []
+# except:
+words = []
+labels = []
+docs_x = []
+docs_y = []
 
-  for intent in data['intents']:
-    for pattern in intent['patterns']:
-      wrds = nltk.word_tokenize(pattern)
-      words.extend(wrds)
-      docs_x.append(wrds)
-      docs_y.append(intent['tag'])
+for intent in data['intents']:
+  for pattern in intent['patterns']:
+    wrds = nltk.word_tokenize(pattern)
+    words.extend(wrds)
+    docs_x.append(wrds)
+    docs_y.append(intent['tag'])
 
-    if intent['tag'] not in labels:
-      labels.append(intent['tag'])
+  if intent['tag'] not in labels:
+    labels.append(intent['tag'])
 
-  words = [stemmer.stem(w.lower()) for w in words if w != "?"]
-  words = sorted(list(set(words)))
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
 
-  labels = sorted(labels)
+labels = sorted(labels)
 
-  training = []
-  output = []
+training = []
+output = []
 
-  out_empty = [0 for _ in range(len(labels))]
+out_empty = [0 for _ in range(len(labels))]
 
-  for x, doc in enumerate(docs_x):
-    bag = []
+for x, doc in enumerate(docs_x):
+  bag = []
 
-    wrds = [stemmer.stem(w) for w in doc]
+  wrds = [stemmer.stem(w) for w in doc]
 
-    for w in words:
-      if w in wrds:
-        bag.append(1)
-      else:
-        bag.append(0)
+  for w in words:
+    if w in wrds:
+      bag.append(1)
+    else:
+      bag.append(0)
 
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
+  output_row = out_empty[:]
+  output_row[labels.index(docs_y[x])] = 1
 
-    training.append(bag)
-    output.append(output_row)
+  training.append(bag)
+  output.append(output_row)
 
-  training = numpy.array(training)
-  output = numpy.array(output)
+training = numpy.array(training)
+output = numpy.array(output)
 
-  with open('data.pickle', 'wb') as f:
-    pickle.dump((words, labels, training, output), f)
-    
+with open('data.pickle', 'wb') as f:
+  pickle.dump((words, labels, training, output), f)
+  
 tensorflow.reset_default_graph()
 
 net = tflearn.input_data(shape=[None, len(training[0])])
@@ -129,7 +129,7 @@ def chat(user_input):
         else:
           return random.choice(responses)
   else:
-    return "Sorry I don't undertand"
+    return "Sorry, I didn't catch what you said. 🤔 Could you please rephrase that?"
 
 def showLinks(prior_user_inputs):
   results = model.predict([bag_of_wrds(prior_user_inputs, words)])[0]
